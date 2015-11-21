62b2a33a020cbd1fec4db5b94faf88327d1ce5e3
Aadesh <aadesh@getsuperapp.com>
2015-11-21 22:22:09 +0530

    added alamofire files

---
+++ FluidCash/FluidCash/Models/Router/AuthRouter.swift
@@ -0,0 +1,49 @@
+//
+//  AuthRouter.swift
+//  candidate
+//
+//  Created by Aditya Vyas on 08/09/15.
+//  Copyright (c) 2015 Super. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+enum AuthRouter: RouterProtocol {
+    
+    case AuthenticateRequest(AuthRequestBody)
+
+    var path: String {
+        switch self {
+            
+            case .AuthenticateRequest:
+                return "/users/login"
+        }
+    }
+    
+    var method: Alamofire.Method {
+        switch self {
+            case .AuthenticateRequest:
+                return .POST
+        
+        }
+    }
+    
+    var parameters: AnyObject? {
+        switch self {
+            
+            default:
+                return nil
+
+        }
+    }
+    
+    var body: RequestBody? {
+        switch self {
+            
+            case .AuthenticateRequest(let candidateFacebookData):
+                return candidateFacebookData
+        }
+    }
+}
